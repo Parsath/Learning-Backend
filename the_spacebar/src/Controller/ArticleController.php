636fb2a0135ac0e06259de936722e7d204db357a
Parsath <bilel.taktak@esprit.tn>
2020-09-22 18:40:22 +0100

    update

---
+++ the_spacebar/src/Controller/ArticleController.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController
+{
+    /**
+     * Route("/")
+     */
+    public function homepage()
+    {
+        return new Response("OMG! My First page already! WOO!");
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            "Future page to show the article: %s",
+            $slug
+        ));
+    }
+
+}
