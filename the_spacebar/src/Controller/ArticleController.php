9975160c4604b531725561cea1db6f0427d6b81e
Parsath <bilel.taktak@esprit.tn>
2020-09-22 19:25:10 +0100

    update

--- the_spacebar/src/Controller/ArticleController.php
+++ the_spacebar/src/Controller/ArticleController.php
@@ -21,13 +21,21 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}")
      * @param $slug
+     * @param $comments
      * @return Response
      */
     public function show($slug)
     {
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
         return $this->render('article/show.html.twig', [
                 'title' => ucwords(str_replace('-',' ',$slug)),
-            ]);
+                'comments' => $comments,
+        ]);
     }
 
 }
